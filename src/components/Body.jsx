58bb6e88edcc64b14f1bff86c76a40cadeff5ac5
Preet Chaudhary <preetchaudhary224@gmail.com>
2025-04-06 15:50:13 +0530

    finish till add feature

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
-import taskdt from "../constants/const1.json"
+
 import Todoform from "./todoform";
 
 const Body = () => {
 const [isopen,setisopen]=useState(false)
+const [tasks, settasks] = useState([])
+useEffect(() => {
+localStorage.setItem("tasks",JSON.stringify(tasks))
+}, [tasks])
+useEffect(() => {
+ const showtasks=JSON.parse(localStorage.getItem("tasks"))
+settasks(showtasks)
+}, [])
+const addtask=(item)=>{
+  if(!item.trim())
+    return ;
+  const latest=[...tasks,{id:Date.now(),value:item}]
+  settasks(latest)
+
+};
 
   return (
     <div className="flex flex-col lg:flex-row justify-between min-h-screen p-4">
@@ -13,20 +28,23 @@ const [isopen,setisopen]=useState(false)
         <h1 className="text-3xl p-2 rounded-2xl bg-slate-300 m-2 text-slate-700">
           Tasks
         </h1>
-        <div className="m-2 flex-col gap-2 p-2 border-2 rounded-2xl border-purple-600 bg-gradient-to-bl from-violet-800 via-pink-500 to-fuchsia-400 w-full  ">
+        <div className="m-2 flex-col gap-2 p-2 border-2 rounded-2xl border-purple-600 bg-gradient-to-l from-violet-500 via-pink-500 to-fuchsia-400 w-full  ">
         
 
-{taskdt.tasks.map(
+{tasks.map(
     (elem)=>(
         
 
-        <div key={elem.id} className="flex gap-1">
+        <div key={elem.id} className="flex gap-3">
               <input
       id="ripple-on"
       type="checkbox"
       className="peer relative h-5 w-5 top-2 cursor-pointer appearance-none rounded border border-slate-300 shadow hover:shadow-md transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-slate-400 before:opacity-0 before:transition-opacity checked:border-slate-800 checked:bg-slate-800 checked:before:bg-slate-400 hover:before:opacity-10"
       />
-            <h1 className="text-3xl font-myfont">{elem.name}</h1>
+            <h1 className="text-3xl font-myfont">{elem.value}</h1>
+            <div className="flex ml-auto gap-2">
+             <button className=" border-2 rounded-2xl p-3 border-2-slate  bg-white">Edit</button><button className="p-1 border-2 border-2-slate   rounded-2xl bg-red-400">Delete</button>
+            </div>
         </div>
     
     )
@@ -40,7 +58,7 @@ const [isopen,setisopen]=useState(false)
         <button onClick={()=>setisopen(true)} className="border-2 border-black p-3 px-6 m-2 rounded-full bg-purple-600">
           Add a task
         </button>
-        {isopen && <Todoform closemodel={()=>{setisopen(false)}} />}
+        {isopen && <Todoform addtask={addtask} closemodel={()=>{setisopen(false)}} />}
       </div>
     </div>
   );
--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react'
 
-const Todoform = (closemodel) => {
+const Todoform = (props) => {
     const [task, settask] = useState("")
+    const handlesubmit=(e)=>{
+      e.preventDefault();
+      props.addtask(task);
+      settask("");
+      props.closemodel();
+
+    }
   return (
     
-    <div className='fixed inset-0 bg-slate-300 bg-opacity-50 flex items-center justify-center '>
-       <div className=' flex gap-2 border-2 rounded-xl border-black p-2 w-3xl'>
-        <form onSubmit={(e)=>e.preventDefault()} className='flex flex-col gap-2 w-full' action="">
+    <div className='fixed inset-0 bg-transparent flex items-center justify-center backdrop-blur-xs '>
+       <div className=' flex gap-2 border-2 bg-purple-300 rounded-xl border-black p-2 w-3xl'>
+        <form onSubmit={handlesubmit} className='flex flex-col gap-2 w-full' action="">
             <h1 className='text-2xl'>Task name</h1>
-            <input className="border-1 border-black rounded-xl p-2 w-full focus:outline-none focus:ring-2 focus:ring-red-200 " placeholder={task==="" && "buy me a pen" } value={task} onChange={(e)=>settask(e.target.value)}  onFocus={null} type="text" />
+            <input  className="border-1 border-black rounded-xl p-2 w-full focus:outline-none focus:ring-2 focus:ring-purple-400 " placeholder={task==="" && "buy me a pen" } value={task} onChange={(e)=>settask(e.target.value)}  onFocus={null} type="text" />
     
-            <button onClick={closemodel.closemodel} className='border-2 items-center justify-center hover:scale-110 rounded-4xl p-2 m-auto border-black bg-amber-500' type='submit'>Add task</button>
+            <button  className='border-2 items-center justify-center hover:scale-110 rounded-4xl p-2 m-auto border-black bg-violet-500' type='submit'>Add task</button>
         </form>
        </div>
     </div>
